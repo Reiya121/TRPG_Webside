4ff7d356129695692615cbd4c3eb83ff360ac0e0
Y04Kato <60003500+Reiya121@users.noreply.github.com>
2026-02-09 16:19:55 +0900

    test

--- data/organizations.js
+++ data/organizations.js
@@ -5,7 +5,7 @@ const organizations = [
     name: "ヴァナルガンド終極技術発展局",
     nameEn: "Vanargand Ultimate Technology Development Bureau",
     faction: '斜歯忍軍',
-    description: "脅威となる妖魔の力を人工的に制御できるシノビを作ろうとしている斜歯忍軍の研究所",
+    description: "脅威となる妖魔の力を人工的に制御できるシノビを作ろうとしている斜歯忍軍の研究所\n【神喰らい計画】という計画を進めており、本来、長い時間を費やして交配により妖魔の血を混ぜる魔王流の手法に着目し、妖魔の遺伝子を直接人体へ投与、適合させることに成功している\nしかし適合確率の低さや、適合後の寿命の激減などの問題が多く指摘されており、今現在の適合者は【第一世代】と呼ばれている",
     logo: "https://github.com/TeamFelNull/TRPG_Website/blob/main/images/organizations/org01.png?raw=true",
     quote: [
       '進化とは、犠牲の上にしか成り立たない',
